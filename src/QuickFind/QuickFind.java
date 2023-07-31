e21eda814cd816692df7ff186908039f86f9afc5
dexterchan <dexterchan@gmail.com>
2023-07-31 21:53:26 +0800

    add quick find eager approach

---
+++ src/QuickFind/QuickFind.java
@@ -0,0 +1,65 @@
+package QuickFind;
+
+public class QuickFind {
+    private int[] id;
+
+    public QuickFind(int N){
+        id = new int[N];
+        for (int i=0;i<N;i++) {
+            id[i] = i;
+        }
+    }
+
+    public boolean connect(int i, int j){
+        return this.id[i] == this.id[j];
+    }
+
+    public void union(int i , int j){
+        if (!this.connect(i,j)) {
+            int prev = this.id[j];
+            int new_value = this.id[i];
+            this.id[j] = new_value;
+
+            //iterate the old prev value and update by j
+            for (int c = 0; c < this.id.length; c++) {
+                if (this.id[c] == prev) {
+                    this.id[c] = new_value;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<this.id.length;i++){
+            if (i<10)
+                sb.append(" ");
+            sb.append(i);
+            sb.append("|");
+        }
+        sb.append("\n");
+
+        for (int i=0; i<this.id.length;i++){
+            if (id[i]<10)
+                sb.append(" ");
+            sb.append(id[i]);
+            sb.append("|");
+        }
+        sb.append("\n");
+
+        return sb.toString();
+    }
+    public static void main(String [] args){
+        QuickFind qf = new QuickFind(10);
+        qf.union(0,5);
+        qf.union(1,2);
+        qf.union(3,4);
+        qf.union(5,6);
+        qf.union(2,7);
+        qf.union(3,8);
+        qf.union(4,9);
+        System.out.println(qf.toString());
+
+    }
+}
