210c236e319623b56a7418af15646ab28541ee4c
dexterchan <dexterchan@gmail.com>
2023-08-21 00:02:30 +0800

    dequeue

--- src/StackAndQueue/Deque.java
+++ src/StackAndQueue/Deque.java
@@ -1,18 +1,64 @@
 package StackAndQueue;
+import utility.NumberArrayCreator;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Stack;
 
 public class Deque<Item> implements Iterable<Item> {
 
-    private class Node{
+    private final class Node{
         Item value;
         Node next;
+        Node prev;
+
         Node(Item value){
             if (value == null){
                 throw new IllegalArgumentException("value cannot be null");
             }
             this.value = value;
-            next = null;
+            next = prev = null;
+        }
+
+        void updateNodeAhead(Node newHeadNode){
+            Node lastHeadNode = this.prev;
+            this.prev = newHeadNode;
+            if (newHeadNode != null ) {
+                newHeadNode.prev = lastHeadNode;
+                newHeadNode.next = this;
+            }
+            if (lastHeadNode!=null)
+                lastHeadNode.next = newHeadNode;
+
+        }
+
+        void updateNodeBehind(Node node){
+            Node lastNextNode = this.next;
+            this.next = node;
+            if (node != null) {
+                node.next = lastNextNode;
+                node.prev = this;
+            }
+            if (lastNextNode != null)
+                lastNextNode.prev = node;
+        }
+
+        void removeMyself(){
+            Node lastNextNode = this.next;
+            Node lastPrevNode = this.prev;
+
+            if (lastNextNode!=null)
+                lastNextNode.prev = lastPrevNode;
+            if (lastPrevNode!=null)
+                lastPrevNode.next = lastNextNode;
+            this.next = null;
+            this.prev = null;
+        }
+
+        @Override
+        public String toString(){
+            String add = (this.next!=null)?this.next.toString():"";
+            return this.value+"->"+ add;
         }
     }
 
@@ -40,12 +86,15 @@ public class Deque<Item> implements Iterable<Item> {
     public void addFirst(Item item){
         Node lastFirst = this.first;
         Node newFirst = new Node(item);
-        this.first = newFirst;
-        newFirst.next = lastFirst;
+
+        if (lastFirst != null)
+            lastFirst.updateNodeAhead(newFirst);
 
         if (this.isEmpty()){
             this.last = newFirst;
         }
+
+        this.first = newFirst;
         this.size += 1;
     }
 
@@ -58,7 +107,7 @@ public class Deque<Item> implements Iterable<Item> {
         if (this.isEmpty()){
             this.first = newLast;
         }else
-            lastLast.next = newLast;
+            lastLast.updateNodeBehind(newLast);
 
         this.size += 1;
     }
@@ -72,9 +121,12 @@ public class Deque<Item> implements Iterable<Item> {
         Item value = old_first.value;
         this.first = old_first.next;
 
+        old_first.removeMyself();
+
         if (this.isEmpty()){
             this.last = null;
         }
+        this.size -= 1;
         return value;
     }
 
@@ -86,19 +138,167 @@ public class Deque<Item> implements Iterable<Item> {
         }
         Node old_last = this.last;
         Item value = old_last.value;
-        this.last = null;
 
+        Node prev_Node = old_last.prev;
+        this.last = prev_Node;
+        old_last.removeMyself();
+
+        if (prev_Node == null){
+            this.first = null;
+        }
+        this.size -= 1;
         return value;
     }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator(){
-        return null;
+        return new Iterator<Item>() {
+            Node ref = Deque.this.first;
+
+            @Override
+            public boolean hasNext() {
+                return ref != null;
+            }
+
+            @Override
+            public Item next() {
+                Item v = ref.value;
+                ref = ref.next;
+                return v;
+            }
+        };
     }
 
     // unit testing (required)
     public static void main(String[] args){
+        testAddFirstElement();
+        testAddFirstLastElement();
+        testAddSeqQueue();
+        testAddSeqStack();
+    }
+
+    public static  void testAddFirstElement(){
+        System.out.println("testAddFirstElement");
+        Deque<Integer> queue = new Deque<>();
+        queue.addFirst(100);
+        assert queue.size()==1:" Not valid";
+        int count_size = 0;
+        for (int i : queue){
+            count_size += 1;
+            System.out.println(i);
+        }
+        assert queue.removeFirst() == 100: "value should be 100";
+        assert queue.isEmpty(): "Not empty";
+
+        count_size = 0;
+        queue.addFirst(200);
+
+        for (int i : queue){
+            count_size += 1;
+            System.out.println(i);
+        }
+        assert count_size == 1;
+        assert queue.removeLast() == 200: "value should be 200";
+        assert queue.isEmpty() && queue.size() == 0;
+
+        queue.addFirst(200);
+        queue.addFirst(100);
+        count_size = 0;
+        for (int i : queue){
+            count_size += 1;
+            System.out.print(i + ",");
+        }
+        System.out.println();
 
     }
+
+    public static void testAddFirstLastElement(){
+        System.out.println("testAddFirstLastElement");
+        Deque<Integer> queue = new Deque<>();
+        int[] values = {100, 200};
+        queue.addLast(values[1]);
+        assert !queue.isEmpty();
+        queue.addFirst(values[0]);
+        int count_items = 0;
+
+        for (int i : queue){
+            System.out.print(i);
+            System.out.print(",");
+            count_items++;
+        }
+        System.out.println();
+        assert count_items == values.length;
+        assert queue.removeLast() == values[values.length-1];
+
+        count_items = 0;
+        for (int i : queue){
+            System.out.print(i);
+            System.out.print(",");
+            count_items++;
+        }
+        assert count_items == 1;
+
+        queue.addLast(300);
+        assert queue.removeLast() == 300;
+
+        queue.addFirst(20);
+        assert queue.removeLast() == 100;
+        assert queue.removeFirst() == 20;
+        System.out.println();
+    }
+
+    public static void testAddSeqQueue(){
+        System.out.println("testAddSeqQueue");
+        NumberArrayCreator creator = new NumberArrayCreator(1);
+        int[] list = creator.create_number_array(10, 1, 100);
+
+        int count_num=0;
+
+        Deque<Integer> queue = new Deque<>();
+        for (int i : list){
+            queue.addLast(i);
+        }
+        System.out.println("queue");
+        for (int i : queue){
+            System.out.print(i+",");
+        }
+        System.out.println();
+        //Now verify if the queue works
+        for (int i: list){
+            count_num += 1;
+            assert queue.removeFirst() == i;
+        }
+        assert count_num == list.length;
+        assert queue.isEmpty();
+
+
+    }
+
+    public static void testAddSeqStack(){
+        System.out.println("testAddSeqStack");
+        NumberArrayCreator creator = new NumberArrayCreator(1);
+        int[] list = creator.create_number_array(10, 1, 100);
+        Stack<Integer> refStack = new Stack<>();
+        Deque<Integer> queue = new Deque<>();
+        //Now verify if the stack works
+        for (int i : list){
+            refStack.add(i);
+            queue.addFirst(i);
+            //System.out.println(queue.first);
+        }
+        System.out.println("stack");
+        assert  !queue.isEmpty();
+        assert queue.size() == refStack.size();
+
+        while (!refStack.isEmpty()){
+            assert refStack.pop() == queue.removeFirst();
+        }
+        assert queue.isEmpty();
+
+    }
+
+
+
+
 
 }
