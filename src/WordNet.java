db34970a430e71eca7118cd9a9990da8ffa1fa3a
dexterchan <dexterchan@gmail.com>
2023-10-30 23:21:18 +0800

    add is noun

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,7 +1,13 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Stack;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,7 +28,7 @@ public class WordNet {
 
             for (int v = 0; v < g.V(); v++) {
                 resetTraverseInfo(g);
-                dfs(g, INVALID_STATE, v);
+                dfs(g, v);
             }
 
         }
@@ -71,7 +77,7 @@ public class WordNet {
             return l.iterator();
         }
 
-        private void dfs(Digraph g, int p, int v) {
+        private void dfs(Digraph g, int v) {
             for (int w : g.adj(v)) {
                 //Short circuit if cycle was found
                 if (hasCycle()) return;
@@ -79,31 +85,30 @@ public class WordNet {
                 if (!this.marked[w]) {
                     this.marked[w] = true;
                     this.edgeTo[w] = v;
-                    dfs(g, v, w);
-                } else{// if (w != p) {
+                    dfs(g, w);
+                } else {// if (w != p) {
                     Stack<Integer> new_cycle = new Stack<>();
                     int x = v;
-                    for (; x!=INVALID_STATE && x != w; x = edgeTo[x]) {
+                    while(x!=INVALID_STATE && x!=w ){
                         new_cycle.push(x);
+                        x = edgeTo[x];
                     }
-                    if (x!=INVALID_STATE){
+
+                    if (x != INVALID_STATE) {
                         new_cycle.push(w);
                         new_cycle.push(v);
                         Iterator<Integer> itr = new_cycle.iterator();
-                        while(itr.hasNext()) this.cycle.push(itr.next());
+                        while (itr.hasNext()) this.cycle.push(itr.next());
                     }
 
                 }
-//                } else {
-//                    System.out.println("Exclude case" + p + "," + v);
-//                }
             }
         }
     }
 
     private static class SymbolTable {
         TreeMap<String, List<Node>> map = new TreeMap<>();
-        private int numNodes = 0;
+        private int max_node_num = 0;
 
         public Iterable<Node> put(String key, Node value) {
             List<Node> values = null;
@@ -114,7 +119,7 @@ public class WordNet {
                 values = this.map.get(key);
             }
             values.add(value);
-            numNodes++;
+            max_node_num = Math.max(max_node_num, value.num);
             return values;
         }
 
@@ -126,8 +131,8 @@ public class WordNet {
             return map.size();
         }
 
-        public int getNumNodes(){
-            return this.numNodes;
+        public int getNumNodes() {
+            return this.max_node_num + 1;
         }
     }
 
@@ -136,6 +141,7 @@ public class WordNet {
         String description;
 
         Node(int num, String description) {
+            if (num < 0) throw new IllegalArgumentException();
             this.num = num;
             this.description = description;
         }
@@ -152,6 +158,7 @@ public class WordNet {
         }
 
         this.symbolTables = WordNet.readSynsetsFile(synsets);
+        this.digraph = WordNet.createDiGraph(this.symbolTables, hypernyms);
         //this.readHyperNyms(hypernyms);
 
     }
@@ -171,7 +178,7 @@ public class WordNet {
             if (numbers.length < 1) {
                 throw new IllegalArgumentException("line" + count + ":edge entry format problem");
             }
-            if(numbers.length == 1){
+            if (numbers.length == 1) {
                 continue;
             }
             int v = Integer.parseInt(numbers[0]);
@@ -193,10 +200,13 @@ public class WordNet {
         //Check if root exits
         int num_outdegree_zero = 0;
         for (int i = 0; i < num_vertex; i++) {
-            if (digraph.outdegree(i) == 0) num_outdegree_zero++;
-            //System.out.println("Root vertex=" + i);
+            if (digraph.outdegree(i) == 0) {
+                System.out.println("Root vertex=" + i);
+                num_outdegree_zero++;
+            }
+
         }
-        System.out.println("Detected "+num_outdegree_zero+" root vertex");
+        System.out.println("Detected " + num_outdegree_zero + " root vertex");
         if (num_outdegree_zero != 1) throw new IllegalArgumentException("No root found");
 
         //Check if cyclic found
@@ -230,12 +240,19 @@ public class WordNet {
             assert count == inx;
             count++;
         }
-        System.out.println("Read " + count + "vertex");
+        System.out.println("Read " + count + " vertex, Max Node number=" + symTable.getNumNodes());
         assert symTable.getNumNodes() == count;
         fileIn.close();
         return symTable;
     }
 
+    private Set<Integer> getNounSynset(){
+        Iterable<Node> nouns = this.symbolTables.get("noun");
+        Iterator<Node> itr= nouns.iterator();
+        HashSet<Integer> nouns_nums = new HashSet<>();
+        while(itr.hasNext()) nouns_nums.add(itr.next().num);
+        return nouns_nums;
+    }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
@@ -244,6 +261,42 @@ public class WordNet {
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
+
+        Iterable<Node> symNums = this.symbolTables.get(word);
+        Set<Integer> nounSet = this.getNounSynset();
+        
+        for(Node node: symNums){
+            Stack<Integer> nounStackTrace = new Stack<>();
+            nounStackTrace.push(node.num);
+            boolean isNoun = this.dfs_check_ancestor(node.num, nounSet, nounStackTrace);
+            if(isNoun){
+                printStackTrace(nounStackTrace);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void printStackTrace(Stack<Integer> stack){
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+    }
+
+    private boolean dfs_check_ancestor(int v, Set<Integer> ancestors, Stack<Integer> path){
+
+        for (int ancestor:this.digraph.adj(v)){
+            if (ancestors.contains(ancestor)){
+                path.push(ancestor);
+                return true;
+            }else{
+                boolean found = dfs_check_ancestor(ancestor, ancestors, path);
+                if (found){
+                    path.push(ancestor);
+                }
+            }
+        }
         return false;
     }
 
@@ -262,29 +315,62 @@ public class WordNet {
     public static void main(String[] args) {
         testCycleGraph();
 
-        var symboltables = testReadSymbSet(args[0]);
-        var Digraph = testCreateDigraph(symboltables, args[1]);
+        //var symboltables = testReadSymbSet(args[0]);
+        //var Digraph = testCreateDigraph(symboltables, args[1]);
 
+        testNoun(args[0], args[1]);
 
     }
 
-    static SymbolTable testReadSymbSet(String file) {
+    private static WordNet testNoun(String synsets, String hypernyms){
+        WordNet wordNet= new WordNet(synsets, hypernyms);
+
+        assert wordNet.isNoun("apple");
+
+        assert  !wordNet.isNoun("running");
+
+        return wordNet;
+    }
+
+    private static SymbolTable testReadSymbSet(String file) {
         SymbolTable st = WordNet.readSynsetsFile(file);
         System.out.println("Number of " + st.size());
         assert st.size() > 0;
         return st;
     }
 
-    static Digraph testCreateDigraph(SymbolTable st, String hyper) {
+    private static Digraph testCreateDigraph(SymbolTable st, String hyper) {
         Digraph graph = WordNet.createDiGraph(st, hyper);
         assert graph != null;
         return graph;
     }
 
-    static void testCycleGraph() {
+    private static void printCycle(DiGraphCycle cycle){
+        Iterator<Integer> c_itr = cycle.cycle();
+        while (c_itr.hasNext()) {
+            System.out.print(c_itr.next() + ",");
+        }
+        System.out.println();
+    }
+    private static void testCycleGraph() {
         Digraph triangle, arbitary;
         DiGraphCycle cycle;
-        Iterator<Integer> c_itr;
+
+        arbitary = new Digraph(3);
+        arbitary.addEdge(1, 2);
+        arbitary.addEdge(2, 1);
+        arbitary.addEdge(2, 0);
+        cycle = new DiGraphCycle(arbitary);
+        assert cycle.hasCycle();
+        printCycle(cycle);
+
+
+        arbitary = new Digraph(3);
+        arbitary.addEdge(0, 1);
+        arbitary.addEdge(1, 0);
+        cycle = new DiGraphCycle(arbitary);
+        assert cycle.hasCycle();
+        printCycle(cycle);
 
         arbitary = new Digraph(3);
         arbitary.addEdge(0, 1);
@@ -292,11 +378,7 @@ public class WordNet {
         arbitary.addEdge(2, 1);
         cycle = new DiGraphCycle(arbitary);
         assert cycle.hasCycle();
-        c_itr = cycle.cycle();
-        while (c_itr.hasNext()) {
-            System.out.print(c_itr.next() + ",");
-        }
-        System.out.println();
+        printCycle(cycle);
 
 
         arbitary = new Digraph(8);
@@ -311,11 +393,7 @@ public class WordNet {
         arbitary.addEdge(7, 5);
         cycle = new DiGraphCycle(arbitary);
         assert cycle.hasCycle();
-        c_itr = cycle.cycle();
-        while (c_itr.hasNext()) {
-            System.out.print(c_itr.next() + ",");
-        }
-        System.out.println();
+        printCycle(cycle);
 
         triangle = new Digraph(3);
         triangle.addEdge(0, 1);
@@ -323,11 +401,7 @@ public class WordNet {
         triangle.addEdge(2, 0);
         cycle = new DiGraphCycle(triangle);
         assert cycle.hasCycle();
-        c_itr = cycle.cycle();
-        while (c_itr.hasNext()) {
-            System.out.print(c_itr.next() + ",");
-        }
-        System.out.println();
+        printCycle(cycle);
 
         triangle = new Digraph(3);
         triangle.addEdge(0, 1);
@@ -354,11 +428,7 @@ public class WordNet {
         arbitary.addEdge(4, 1);
         cycle = new DiGraphCycle(arbitary);
         assert cycle.hasCycle();
-        c_itr = cycle.cycle();
-        while (c_itr.hasNext()) {
-            System.out.print(c_itr.next() + ",");
-        }
-        System.out.println();
+        printCycle(cycle);
 
     }
 }
