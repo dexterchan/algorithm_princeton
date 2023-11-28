1ee97d402c221beaac5da00752ca0ae780944c88
wchan436 <wchan436@bloomberg.net>
2023-11-28 18:11:51 +0800

    sub string DFA

---
+++ src/SubString/KMP_DFA.java
@@ -0,0 +1,65 @@
+package SubString;
+
+public class KMP_DFA {
+    final static int CHAR_SIZE = 256; //ASCII
+    final int[][] dfa;
+    final String pattern;
+
+    public KMP_DFA(String pattern) {
+        this.pattern = pattern;
+        dfa = new int[CHAR_SIZE][pattern.length()];
+
+        resetDfa(dfa);
+        fillTransition(dfa, this.pattern);
+    }
+
+    private static int[][] resetDfa(int[][] dfa) {
+        for (int i = 0; i < dfa.length; i++) {
+            for (int j = 0; j < dfa[i].length; j++) dfa[i][j] = 0;
+        }
+        return dfa;
+    }
+
+    private static int[][] fillTransition(int[][] dfa, String pattern) {
+        int match_case = getPatternIndex(pattern, 0);
+        dfa[match_case][0] = 1;
+        for (int j = 1, X = 0; j < pattern.length(); j++) {
+            for (int c = 0; c < CHAR_SIZE; c++) {
+                dfa[c][j] = dfa[c][X];
+            }
+            match_case = getPatternIndex(pattern, j);
+            dfa[match_case][j] = j + 1;
+            X = dfa[match_case][X];
+        }
+        return dfa;
+    }
+
+    public int getNexState(int stage, char state){
+        return this.dfa[state][stage];
+    }
+
+    private static int getPatternIndex(String pattern, int i) {
+        if (i < 0 || i >= pattern.length()) throw new IllegalArgumentException();
+        return pattern.charAt(i);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.pattern);
+        sb.append("\n");
+        for (int i = 'A'; i < this.dfa.length && i <= 'Z'; i++) {
+            for (int j = 0; j < this.dfa[i].length; j++) {
+                sb.append(this.dfa[i][j]);
+                sb.append(",");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        KMP_DFA dfa = new KMP_DFA("ABABAC");
+        System.out.println(dfa.toString());
+    }
+}
--- src/SubString/SubStringTest.java
+++ src/SubString/SubStringTest.java
@@ -7,7 +7,7 @@ public class SubStringTest {
         int i = 0, N = text.length();
         int j = 0, M = pat.length();
 
-        for ( i = 0, j = 0; i < N && j < M; i++) {
+        for (i = 0, j = 0; i < N && j < M; i++) {
             if (text.charAt(i) == pat.charAt(j)) j++;
             else {
                 i -= j;
@@ -18,10 +18,30 @@ public class SubStringTest {
         else return -1;
     }
 
+
+    public static int kmpDfaSubstringSearch(String pat, String text) {
+        int N = text.length();
+        int M = pat.length();
+        int i, j;
+        KMP_DFA dfa = new KMP_DFA(pat);
+        for (i = 0, j = 0; i < N && j < M; i++) {
+            char c = text.charAt(i);
+            j = dfa.getNexState(j, c);
+        }
+        if (j == M) return i - M;
+        else return -1;
+    }
+
     public static void main(String args[]) {
         String text = "ABACADABRAC";
         String pat = "ADABR";
-        System.out.println(bruteForceSubstringSearch(pat, text));
+        assert (bruteForceSubstringSearch(pat, text))==4;
+        pat = "BRAC";
+        assert (kmpDfaSubstringSearch(pat, text)) == 7;
+
+        pat = "AAAAA";
+        assert (kmpDfaSubstringSearch(pat, text)) == -1;
+
     }
 
 }
